c54799fc5b3317431eb0d02bfa0d6d0456858e29
Michael Ballard <ballard.michael9@gmail.com>
2018-05-09 17:56:26 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -37,6 +37,6 @@ end
 
 def shortened_tweet_truncator(current_tweet)
   clean_tweet = selective_tweet_shortener(current_tweet)
-  clean_tweet.length > 140 ? clean_tweet[0..136] << "..." : clean_tweet_tweet
+  clean_tweet.length > 140 ? clean_tweet[0..136] << "..." : clean_tweet
 end
   
