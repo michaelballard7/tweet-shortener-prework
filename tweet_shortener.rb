af7a50ea5e0617203655a8c3c936e366a0c1e552
Michael Ballard <ballard.michael9@gmail.com>
2018-05-09 17:10:21 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,9 +1,11 @@
 # Write your code here.
 
 def dictionary 
-  substitutes = {
+  {
     "hello" => "hi",
-    "to, two, too" => "2",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
     "for, four" => "4",
     "be" => 'b',
     "you" => "u",
